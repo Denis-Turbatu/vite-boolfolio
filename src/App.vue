e23397ea50a6e16fee047c12ecbfe01093ba9ef3
Denis-Turbatu <turbatudenis81@gmail.com>
2024-06-28 14:24:07 +0200

    feat: axios API call + console.log for debug

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,26 @@
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      projects: [],
+    }
+  },
+  created() {
+    axios.get('http://127.0.0.1:8000/api/projects').then((resp) => {
+      console.log(resp.data.result);
+    });
+  },
+}
+</script>
+
 <template>
   <div>
     
   </div>
 </template>
-<script>
-export default {
-  
-}
-</script>
+
 <style scoped lang="scss">
   
 </style>
