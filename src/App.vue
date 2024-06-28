4f0f1ecd84a2733b418ea1c13523ce6b9b08f502
Denis-Turbatu <turbatudenis81@gmail.com>
2024-06-28 15:19:37 +0200

    feat: make component for cards + props passage and show list projects

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,36 @@
 <script>
-import axios from 'axios';
+import axios from "axios";
+import AppProjectCard from "./components/AppProjectCard.vue";
 
 export default {
+  components: {
+    AppProjectCard
+  },
   data() {
-    return {
-      projects: [],
+    components: {
+      AppProjectCard;
     }
+    return {
+      projects: []
+    };
   },
   created() {
-    axios.get('http://127.0.0.1:8000/api/projects').then((resp) => {
-      console.log(resp.data.result);
+    axios.get("http://127.0.0.1:8000/api/projects").then((resp) => {
+      //console.log(resp.data.result);
+      this.projects = resp.data.result;
     });
-  },
-}
+  }
+};
 </script>
 
 <template>
-  <div>
-    
+  <div class="container">
+    <div class="row">
+      <div class="col col-md-6 col-lg-4" v-for="card in projects" :key="card">
+        <AppProjectCard :cardObj="card" />
+      </div>
+    </div>
   </div>
 </template>
 
-<style scoped lang="scss">
-  
-</style>
+<style scoped lang="scss"></style>
---
+++ src/components/AppProjectCard.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+    props: {
+        cardObj: Object,
+    }
+};
+</script>
+
+<template>
+    <div>
+        <div class="card border-2" style="width: 18rem">
+            <img :src="cardObj.thumb ? `${cardObj.thumb}` : 'https://via.placeholder.com/640x480.png'"
+                class="card-img-top" :alt="`${cardObj.title}`" />
+            <div class="card-body">
+                <section class="card-text">
+                    <section>
+                        <h4>{{ cardObj.title }}</h4>
+                        <p class="mb-3">{{ cardObj.description }}</p>
+                        <p>{{ cardObj.slug }}</p>
+                    </section>
+
+                </section>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped lang="scss"></style>
