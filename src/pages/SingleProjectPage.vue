d6e50bae3840c8b851bb935b2a3b99a8ef9c4bb5
Denis-Turbatu <turbatudenis81@gmail.com>
2024-07-02 12:51:07 +0200

    feat: show data of single project

--- src/pages/SingleProjectPage.vue
+++ src/pages/SingleProjectPage.vue
@@ -1,19 +1,25 @@
 <script>
+import axios from "axios";
     export default {
         data() {
             return {
-                
-            }
+                project: null,
+            };
         },
         created() {
-            console.log(this.$route.params.slug);
+            const slug = this.$route.params.slug;
+
+            axios.get(`http://127.0.0.1:8000/api/projects/${slug}`).then((resp) =>{
+                this.project = resp.data.result;
+            });
         },
     };
 </script>
 
 <template>
     <div>
-    
+    <h1>{{project.title}}</h1>
+    <p>{{project.description}}</p>
     </div>
 </template>
 
