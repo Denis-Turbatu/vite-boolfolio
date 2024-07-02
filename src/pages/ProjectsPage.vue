58d59c80e453b3d7ceda3192592c20e694579a9e
Denis-Turbatu <turbatudenis81@gmail.com>
2024-07-02 19:35:58 +0200

    feat: implemented paginator with bootstrap + created functions changePage() to update the current page and getProjects to update data inside the cards

--- src/pages/ProjectsPage.vue
+++ src/pages/ProjectsPage.vue
@@ -11,15 +11,33 @@ export default {
       AppProjectCard;
     }
     return {
-      projects: []
+      projects: [],
+      lastPage: null,
+      currentPage: null,
     };
   },
   created() {
     axios.get("http://127.0.0.1:8000/api/projects").then((resp) => {
-      //console.log(resp.data.result);
-      this.projects = resp.data.result;
+      // console.log(resp.data.result);
+      // console.log("currentPage", resp.data.result.current_page);
+      // console.log("lastPage", resp.data.result.last_page);
+      this.projects = resp.data.result.data;
+      this.lastPage = resp.data.result.last_page;
+      this.currentPage = resp.data.result.last_page;
     });
-  }
+  },
+  methods: {
+    getProjects(pageNumber) {
+      axios.get(`http://127.0.0.1:8000/api/projects?page=${pageNumber}`).then((resp) => {
+        console.log(resp.data.result);
+        console.log("currentPage", resp.data.result.current_page);
+        this.projects = resp.data.result.data;
+      });
+    },
+    changePage(pageNumber) {
+      this.getProjects(pageNumber);
+    }
+  },
 };
 </script>
 
@@ -33,8 +51,18 @@ export default {
         <AppProjectCard :cardObj="card" />
       </div>
     </div>
+    <nav aria-label="Page navigation example">
+      <ul class="pagination">
+        <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+        <li class="page-item" v-for="page in lastPage" :key="page">
+          <a class="page-link" href="#" @click="changePage(page)">{{ page }}</a>
+        </li>
+        <li class="page-item"><a class="page-link" href="#">Next</a></li>
+      </ul>
+    </nav>
   </div>
 </template>
 
-<style scoped lang="scss">
-</style>
+<style scoped lang="scss"></style>
+
+<!-- @click.prevent="changePage(num_page)" -->
